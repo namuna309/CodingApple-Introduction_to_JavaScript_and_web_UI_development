0565e14995635b0c46611c18e6f035ae81a69343
namuna309 <nyah309@gmail.com>
2023-11-17 11:32:29 +0900

    Array 와 Object 자료형 + 숙제

--- tab.js
+++ tab.js
@@ -8,11 +8,35 @@ var tabNum = $('.tab-button').length;
 var tabButton = $('.tab-button');
 var tabContent = $('.tab-content')
 
-for (let i = 0; i < tabNum; i++) {
-    $('.tab-button').eq(i).on('click', () => {
-        tabButton.removeClass('orange');
-        tabContent.removeClass('show');
-        tabButton.eq(i).addClass('orange');
-        tabContent.eq(i).addClass('show');
-    });
+// for (let i = 0; i < tabNum; i++) {
+//     openTab(i);
+// }
+
+$('.list').click((e) => {
+    // 지금 누른게 버튼 0이면 탭열기 0
+    // ...
+    // for (let i = 0; i < tabNum; i++) {
+    //     if( $('.tab-button').eq(i).is($(e.target))) {
+    //         openTab(i)
+    //     }
+
+    // }
+    // dataset활용
+    openTab(e.target.dataset.id)
+})
+
+// function openTab(index) {
+//     $('.tab-button').eq(index).on('click', () => {
+//         tabButton.removeClass('orange');
+//         tabContent.removeClass('show');
+//         tabButton.eq(index).addClass('orange');
+//         tabContent.eq(index).addClass('show');
+//     });
+// }
+
+function openTab(index) {
+    tabContent.removeClass('show');
+    tabButton.removeClass('orange');
+    tabContent.eq(index).addClass('show');
+    tabButton.eq(index).addClass('orange');
 }
